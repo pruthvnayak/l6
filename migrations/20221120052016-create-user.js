917a97674a34f841fa9f196ccc4334f2b9380b38
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-08 10:37:55 +0530

    CHANGED

--- migrations/20221120052016-create-user.js
+++ migrations/20221120052016-create-user.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      
+    
       firstName: {
         type: Sequelize.STRING,
         allowNull: false,
